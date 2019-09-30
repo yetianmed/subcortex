7936f7c9c3e3236a1cbdd4326b14c0a307e5a61b
yetianmed <55817135+yetianmed@users.noreply.github.com>
2019-10-01 09:52:15 +1000

    Update task_regressor.m

--- functions/task_regressor.m
+++ functions/task_regressor.m
@@ -1,23 +1,25 @@
 function regressor=task_regressor(TR,T,taskfile)
-% clear all
-% close all
-%addpath('./functions')
-%If using the Custom 3 column format, prepare a plain text file (avoiding editors such as MS Word) 
+
+%taskfile: If using the Custom 3 column format, prepare a plain text file (avoiding editors such as MS Word) 
 %containing one line for each stimulus associated with the condition to be modelled by this EV. 
 %Each line must contain three numbers: the onset time (in seconds); the duration (in seconds); 
 %the relative magnitude of each stimulus (usually set to 1) 
+% Example: 
+% taskfile='fear.txt';
+% 32.08	    18	1
+% 74.223	18	1
+% 116.365	18	1
+
+%TR: in seconds
+%T: number of frames
 
 %Convolve with HRF
 Convolve=1; %0=no 1=yes
 
-
-%TR=0.72; %seconds
-%T=176;   %number of frames
 hrf=spm_hrf(TR); 
 
-%Fear Regressor
+%Regressor
 regressor=zeros(T,1);
-%taskfile='fear.txt';
 t=dlmread(taskfile);
 for i=1:size(t,1)
     start=t(i,1); 
